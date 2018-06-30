bd70381c2c616bc503194b408342ca032e760170
NieSu <niesuisontheway@gmail.com>
2018-06-30 15:34:04 +0800

    初次提交

---
+++ src/main/java/com/su/PheryApplication.java
@@ -0,0 +1,14 @@
+package com.su;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PheryApplication
+{
+
+  public static void main(String[] args)
+  {
+    SpringApplication.run(PheryApplication.class, args);
+  }
+}
