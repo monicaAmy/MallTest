d767c5c20078c3ab39c5537ab8dd8ef938a08b74
NieSu <niesuisontheway@gmail.com>
2018-07-01 21:24:22 +0800

    AOP切面还要配置扫描才行!!!

--- pheryweb/src/main/java/com/su/pheryweb/controller/UserController.java
+++ pheryweb/src/main/java/com/su/pheryweb/controller/UserController.java
@@ -2,6 +2,9 @@ package com.su.pheryweb.controller;
 
 import com.su.pherycommon.util.AES;
 import com.su.pherycommon.util.SystemControllerLog;
+import com.su.pherycommon.util.SystemLogAspect;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value="/user")
 public class UserController
 {
+
   @PostMapping("/uu")
-  @SystemControllerLog(description = "haha")
+  //@SystemControllerLog(description = "haha")
   @AES
   public String uu()
   {
